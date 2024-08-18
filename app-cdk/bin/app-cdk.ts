a8b30cd0bbc1856eca838e42e9b9e72cbdc322ec
Miguel Angel Pulgar Flores <mpulgar@activeit.cl>
2024-08-18 16:12:17 -0400

    subir s3 imagen

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,9 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { PipelineCdkStack  } from '../lib/pipeline-cdk-stack';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
 const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+
 const pipelineCdkStack = new PipelineCdkStack (app, 'PipelineCdkStack', {
     ecrRepository: ecrCdkStack.repository,
   });
+
+ 
