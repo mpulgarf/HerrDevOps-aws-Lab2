208b243c717244879d938d4f62ebdb03c6ad703f
Miguel Angel Pulgar Flores <mpulgar@activeit.cl>
2024-08-18 18:24:38 -0400

    actualizacion para despliegue.

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,10 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { PipelineCdkStack  } from '../lib/pipeline-cdk-stack';
+import { AppCdkStack } from '../lib/app-cdk-stack';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
 const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
+
+const testCdkStack = new AppCdkStack(app, 'test', {
+  ecrRepository: ecrCdkStack.repository,
+});
 
 const pipelineCdkStack = new PipelineCdkStack (app, 'PipelineCdkStack', {
     ecrRepository: ecrCdkStack.repository,
