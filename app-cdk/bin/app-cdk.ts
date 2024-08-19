d9107510d8715dddcf26c030e60cc3e529084011
Miguel Angel Pulgar Flores <mpulgar@activeit.cl>
2024-08-18 20:17:12 -0400

    ambiente prod.

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -10,6 +10,10 @@ const testCdkStack = new AppCdkStack(app, 'test', {
   ecrRepository: ecrCdkStack.repository,
 });
 
+const prodCdkStack = new AppCdkStack(app, 'prod', {
+  ecrRepository: ecrCdkStack.repository,
+});
+
 const pipelineCdkStack = new PipelineCdkStack (app, 'PipelineCdkStack', {
     ecrRepository: ecrCdkStack.repository,
     fargateServiceTest: testCdkStack.fargateService,
