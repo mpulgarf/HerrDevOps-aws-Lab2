3c0b9143c516f66dedf540d69239c741556ef829
Miguel Angel Pulgar Flores <mpulgar@activeit.cl>
2024-08-18 20:04:45 -0400

    actualizacion web

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Lab2- Herramienta DevOps - Miguel Pulgar</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
